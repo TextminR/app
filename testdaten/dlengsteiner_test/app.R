f05809fa23c838758f35e49b5363e6eb05a0a89a
dlengsteiner@student.tgm.ac.at <dlengsteiner@student.tgm.ac.at>
2023-03-16 18:25:49 +0100

    barchart-shinyapp running successfully

---
+++ testdaten/dlengsteiner_test/app.R
@@ -0,0 +1,52 @@
+library(shiny)
+library(tidyverse)
+library(stringr)
+library(tidytext)
+
+#getting and formatting data
+textdata <- base::readRDS(url("https://slcladal.github.io/data/sotu_paragraphs.rda", "rb"))
+speech <- textdata %>% group_by(speech_doc_id,president,date,) %>% summarise(text = paste0(text, collapse = " ")) 
+
+
+
+ui <- fluidPage(
+  textInput("words_input","Type in words you want to see displayed, seperated by a comma (,). WARNING: words are case-sensitive!"),
+  plotOutput("barchart")
+)
+
+
+
+server <- function(input, output, session) {
+  
+  print("Server starting...")
+  
+  #printing the barchart
+  output$barchart <- renderPlot({
+    #split the input by  comma (,) to extract words
+    words <- strsplit(input$words_input,",")[[1]]
+    
+    
+    #creating vector for the word count of each word
+    wordcount<- c()
+    for(i in 1:length(words)){
+      #Removing all non-alpha-numeric characters and numbers from the extracted words,
+      #if(!(str_replace_all(words[i], "[^[:alnum:]]", ""))==""){
+        words[i] <- str_replace_all(words[i], "[^[:alnum:]]", "")
+        #saving the wordcount of the current word to the new vector
+        wordcount[i] <- sum(str_count(speech$text, words[i]))
+      #}
+    }
+    
+    #dataframe which combines words and wordcount
+    dataframe <- data.frame(word=words,count=wordcount)
+    #plotting the dataframe
+    ggplot(data=dataframe, aes(x=word,y=count)) + 
+      geom_bar(stat="identity", aes(fill=count)) +
+      #scale_fill_gradientn(colours = c("darkblue","blue","blue","purple","magenta","red","red")) + 
+      ggtitle("Frequency of words used in presidential speeches from 1790 - 1903")
+    #barplot(wordcount,names.arg=words,xlab="Word",ylab="Frequency",col="Red",main="Frequency of words used in presidential speeches from 1790 - 1903",border="black")
+  })
+}
+
+#starting the shiny-app
+shinyApp(ui, server)
