45d48cbb9d797d0e7eb6188d8536aa2689a9cb15
yakalin <yakalin@student.tgm.ac.at>
2023-03-22 15:25:05 +0100

    Errorhandling

--- testdaten/Trend/app.R
+++ testdaten/Trend/app.R
@@ -32,41 +32,80 @@ merge_fun <- function(x, y) {
 
 
 ui <- fluidPage(
-    textInput("words", "Wörter eingeben:"), 
-    plotlyOutput("plot")
+    
+    #changing font size of all text
+    tags$head(
+      tags$style("label{font-size: 12pt;}"),
+      tags$style("[type = 'text'] {font-size:12pt;height:20pt;}")
+    ),
+    #title of shiny page
+    titlePanel(
+      "Linecharts"
+    ),
+    #main panel for I/O
+    sidebarLayout(
+      sidebarPanel(
+        textInput("words","Type in words you want to see displayed.")
+      ),
+      mainPanel(
+        plotlyOutput("plot", height = "500px")
+      )
+    )
 )
 
 
 
 
 server <- function(input, output, session) {
-  observeEvent(input$words,{
-    output$plot <- renderPlotly({
+  tryCatch({
+    observeEvent(input$words,{
       words <- strsplit(input$words, ",")[[1]]
-      
-      dfList <- list()
-      i=1
-      for (word in words){
-        if(!(str_replace_all(word, "[^[:alnum:]]", ""))==""){
-          word <- str_replace_all(word, "[^[:alnum:]]", "")
-          dfList[[i]] = plooten(word)
-          i=i+1
+      output$plot <- renderPlotly({
+        
+        dfList <- list()
+        i=1
+        for (word in words){
+          if(!(str_replace_all(word, "[^[:alnum:]]", ""))==""){
+            word <- str_replace_all(word, "[^[:alnum:]]", "")
+            dfList[[i]] = plooten(word)
+            i=i+1
+          }
+          if(i > 3){
+            break
+          }
         }
-        if(i > 3){
-          break
-        }
-      }
+        
+        merged <- Reduce(merge_fun, dfList)
+        
+        tryCatch({
+          merged2 <- melt(merged, id.vars = c("speech_doc_id", "date", "president"))
+          p <- ggplot() +
+            geom_smooth(merged2, mapping = aes(date, value, color = variable), se = FALSE)
+          
+          p <- ggplotly(p)
+          p
+        },error = function(e)
+          ggplot() +                      
+            annotate("text",
+                   x = 1,
+                   y = 1,
+                   size = 12,
+                   label = "Please enter a word") + 
+            theme_void()
+        )
       
-      merged <- Reduce(merge_fun, dfList)
-      
-      merged2 <- melt(merged, id.vars = c("speech_doc_id", "date", "president"))
-      
-      p <- ggplot() +
-        geom_smooth(merged2, mapping = aes(date, value, color = variable), se = FALSE)
-      
-      p <- ggplotly(p)
-      p
+        
+
+      })
     })
-  })
+  },error=function(e)
+    ggplotly() +                      
+    annotate("text",
+             x = 1,
+             y = 1,
+             size = 12,
+             label = "Please enter a word") + 
+    theme_void()
+  )
 }
 shinyApp(ui, server)
